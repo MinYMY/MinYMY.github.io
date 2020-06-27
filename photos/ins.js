a2f9bce0e5e9492cad75137289ce809ac4437202
System Administrator <root@yumindeMacBook-Pro.local>
2020-06-27 22:08:39 +0800

    Site updated: 2020-06-27 22:08:39

--- photos/ins.js
+++ photos/ins.js
@@ -116,8 +116,8 @@
         var data = res.list[j].arr;
         var liTmpl = "";
         for (var i = 0, len = data.link.length; i < len; i++) {
-          var minSrc = 'https://raw.githubusercontent.com/MinYMY/hello-world/master/min_photos/' + data.link[i];
-          var src = 'https://raw.githubusercontent.com/MinYMY/hello-world/master/photos/' + data.link[i];
+          var minSrc = 'https://raw.githubusercontent.com/MinYMY/blog_album/master/min_photos/' + data.link[i];
+          var src = 'https://raw.githubusercontent.com/MinYMY/blog_alnum/master/photos/' + data.link[i];
           var type = data.type[i];
           var target = src + (type === 'video' ? '.mp4' : '.jpg');
           src += '';
